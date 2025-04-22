58582421a4e79fb2c1444a6222cf54eb24de5718
C-Radius <deus.deceit@gmail.com>
2025-04-22 17:02:29 +0300

    Idon't know what i did but updating

--- init.lua
+++ init.lua
@@ -33,13 +33,6 @@ require("lazy").setup({
     { "neoclide/coc.nvim", branch = "release" },
     { "nvim-lua/plenary.nvim" },
     { "nvim-telescope/telescope.nvim", tag = "0.1.8" },
-    {
-        "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit for the latest features
-        config = function()
-            require("nvim-surround").setup({})
-        end,
-    },
     { "windwp/nvim-autopairs" },
     { "rcarriga/nvim-notify" },
     {
@@ -129,50 +122,60 @@ require("lazy").setup({
         ---@module "neo-tree"
         ---@type neotree.Config?
         opts = {
-            -- fill any relevant options here
+            filesystem = {
+                filtered_items = {
+                    visible = true
+                }
+            }
         },
     },
-    { "echasnovski/mini.surround", version = '*',
-        -- Add custom surroundings to be used on top of builtin ones. For more
-        -- information with examples, see `:h MiniSurround.config`.
-        custom_surroundings = nil,
+    {
+      "echasnovski/mini.surround",
+      version = "*", -- or a specific version/tag if needed
+      config = function()
+        require("mini.surround").setup({
+            -- Add custom surroundings to be used on top of builtin ones. For more
+            -- information with examples, see `:h MiniSurround.config`.
+            custom_surroundings = nil,
 
-        -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
-        highlight_duration = 500,
+            -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+            highlight_duration = 500,
 
-        -- Module mappings. Use `''` (empty string) to disable one.
-        mappings = {
-            add = 'sa', -- Add surrounding in Normal and Visual modes
-            delete = 'sd', -- Delete surrounding
-            find = 'sf', -- Find surrounding (to the right)
-            find_left = 'sF', -- Find surrounding (to the left)
-            highlight = 'sh', -- Highlight surrounding
-            replace = 'sr', -- Replace surrounding
-            update_n_lines = 'sn', -- Update `n_lines`
+            -- Module mappings. Use `''` (empty string) to disable one.
+            mappings = {
+                add = 'sa', -- Add surrounding in Normal and Visual modes
+                delete = 'sd', -- Delete surrounding
+                find = 'sf', -- Find surrounding (to the right)
+                find_left = 'sF', -- Find surrounding (to the left)
+                highlight = 'sh', -- Highlight surrounding
+                replace = 'sr', -- Replace surrounding
+                update_n_lines = 'sn', -- Update `n_lines`
 
-            suffix_last = 'l', -- Suffix to search with "prev" method
-            suffix_next = 'n', -- Suffix to search with "next" method
-        },
+                suffix_last = 'l', -- Suffix to search with "prev" method
+                suffix_next = 'n', -- Suffix to search with "next" method
+            },
 
-        -- Number of lines within which surrounding is searched
-        n_lines = 20,
+            -- Number of lines within which surrounding is searched
+            n_lines = 20,
 
-        -- Whether to respect selection type:
-        -- - Place surroundings on separate lines in linewise mode.
-        -- - Place surroundings on each line in blockwise mode.
-        respect_selection_type = false,
+            -- Whether to respect selection type:
+            -- - Place surroundings on separate lines in linewise mode.
+            -- - Place surroundings on each line in blockwise mode.
+            respect_selection_type = false,
 
-        -- How to search for surrounding (first inside current line, then inside
-        -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
-        -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
-        -- see `:h MiniSurround.config`.
-        search_method = 'cover',
+            -- How to search for surrounding (first inside current line, then inside
+            -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+            -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+            -- see `:h MiniSurround.config`.
+            search_method = 'cover',
 
-        -- Whether to disable showing non-error feedback
-        -- This also affects (purely informational) helper messages shown after
-        -- idle time if user input is required.
-        silent = false,
-    }
+            -- Whether to disable showing non-error feedback
+            -- This also affects (purely informational) helper messages shown after
+            -- idle time if user input is required.
+            silent = false,
+        })
+      end,
+	}
 })
 
 -- Environment variables
@@ -260,6 +263,7 @@ vim.api.nvim_set_keymap("t", "<M-k>", "[[<C-w>h]]", {noremap = true, silent = tr
 vim.api.nvim_set_keymap("t", "<M-j>", "[[<C-w>j]]", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "bn", ":bnext<CR>", {})
 vim.api.nvim_set_keymap("n", "bp", ":bprev<CR>", {})
+vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>', { noremap = true, silent = true })
 
 -- Telescope Keybindings
 vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true, silent = true })
