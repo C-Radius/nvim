05bae0fa4694e893e79a1cd912b964f27c8f267f
C-Radius <deus.deceit@gmail.com>
2025-05-10 14:08:49 +0300

    Went back to cock, discarding everythign i did through trying to change to lsp. it's seriously not worth it. reverted, tidied up a little bit, and now everythign is gucci... more to come

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -11,17 +11,19 @@ require("lazy").setup({
     { "github/copilot.vim" },
 
     -- Configured plugins
-    require("plugins.config_github_nvim_theme"),
-    require("plugins.config_window_picker"),
-    require("plugins.config_telescope"),
-    require("plugins.config_which_key"),
-    require("plugins.config_treesitter"),
-    require("plugins.config_nvim_dbee"),
-    require("plugins.config_neotree"),
-    require("plugins.config_undotree"),
-    require("plugins.config_coc"),
-    require("plugins.config_toggleterm"),
-    require("plugins.config_mini_surround"),
+    require("plugins.github-nvim-theme"),
+    require("plugins.window-picker"),
+    require("plugins.telescope"),
+    require("plugins.which-key"),
+    require("plugins.treesitter"),
+    require("plugins.nvim-dbee"),
+    require("plugins.neotree"),
+    require("plugins.undotree"),
+    require("plugins.coc"),
+    require("plugins.toggleterm"),
+    require("plugins.mini-surround"),
+    require("plugins.lualine"),
+    require("plugins.dashboard-nvim"),
 })  
 
 -- In the case that there's configuration between plugins 
@@ -29,5 +31,5 @@ require("lazy").setup({
 -- we use a plugins bridge directory and store a file for each bridging of plugins. 
 -- This statment includes all files from the plugins bridge directory.
 pcall(function()
-  require("utils.loader").require_all_from("plugins.plugin_bridge")
+  require("utils.loader").require_all_from("plugins.plugin-bridge")
 end)
