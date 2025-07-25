c5fdf2943b37966f1f8128c403586fb857b71a99
C-Radius <deus.deceit@gmail.com>
2025-07-25 14:14:03 +0300

    Added some more plugins for completion

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,13 +1,17 @@
 return {
     "hrsh7th/nvim-cmp",
     dependencies = {
-        "hrsh7th/cmp-nvim-lsp",         -- LSP source
-        "hrsh7th/cmp-buffer",           -- buffer words
-        "hrsh7th/cmp-path",             -- file paths
-        "hrsh7th/cmp-cmdline",          -- command-line completion
-        "saadparwaiz1/cmp_luasnip",     -- LuaSnip source
-        "L3MON4D3/LuaSnip",             -- snippet engine
-        "rafamadriz/friendly-snippets", -- common snippets (vscode-style)
+        "hrsh7th/cmp-nvim-lsp",                -- LSP source
+        "hrsh7th/cmp-vsnip",                   -- LSP source
+        "hrsh7th/nvim-cmp",                    -- LSP source
+        "hrsh7th/cmp-nvim-lua",                -- LSP source
+        "hrsh7th/cmp-buffer",                  -- buffer words
+        "hrsh7th/cmp-nvim-lsp-signature-help", -- buffer words
+        "hrsh7th/cmp-path",                    -- file paths
+        "hrsh7th/cmp-cmdline",                 -- command-line completion
+        "saadparwaiz1/cmp_luasnip",            -- LuaSnip source
+        "L3MON4D3/LuaSnip",                    -- snippet engine
+        "rafamadriz/friendly-snippets",        -- common snippets (vscode-style)
     },
 
     config = function()
